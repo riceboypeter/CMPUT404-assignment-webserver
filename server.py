c9fd40e6c0cd400fb5d118de21254b1b072c4601
Peter <sqiu@ualberta.ca>
2023-01-27 13:13:48 -0700

    i'm like a good 90% sure we're all good here

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 #  coding: utf-8 
-import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +25,129 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+import socketserver
+import os
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    # all the error handlers return an HTML body for an error page with a rickroll
+    def handle_404(self):
+        body = "HTTP/1.1 404 Not Found\nContent-Type: text/html\n\n"
+        body += "<HTML><body>Error 404: Page not found!"
+        body += "<p>Maybe you were looking for <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>this page?</a></p></body></HTML>\n"
+
+        return body
+
+    def handle_405(self):
+        body = "HTTP/1.1 405 Method Not Allowed\nContent-Type: text/html\n\n"
+        body += "<HTML><body>Error 405: Method not allowed! But you can bet that Rick is allowed!<br>"
+        body += "<iframe width='420' height='345' src='https://www.youtube.com/watch?v=dQw4w9WgXcQ'></iframe></body></HTML>\n"
+
+        return body
+
+    def handle_301(self,path):
+        body = "HTTP/1.1 301 Moved Permanently\nContent-Type: text/html\n\n"
+        body += "<HTML><body>Error 301: Moved Permanently!<br>"
+        body += "New location: <a href='/{path}/'>127.0.0.1/{path}/</a><br>".format(path=path[-1])
+        body += "You should also move permanently to Rick instead!<br>"
+        body += "<iframe width='420' height='345' src='https://www.youtube.com/watch?v=dQw4w9WgXcQ'></iframe></body></HTML>\n"
+
+        return body
+
+    # maybe the rickroll is getting old...
+    # just trying to get back at Hindle for getting us in class smh
+
+    # anyways here the HTML is returned if it's all good, or a 404 if you're naughty
+    def handle_html(self,path):
+        body = ""
+        try:
+            html = open(path)
+            print("is anything here?")
+            if html:
+                print("well?")
+                body += "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
+                body += html.read()
+                html.close()
+        except:
+            print("yo that's a 404")
+            body += self.handle_404()
+        return body
+
+    # and here is the handler for CSS
+    def handle_css(self,path):
+        body = ""
+        try:
+            css = open(path)
+            print("is anything here?")
+            if css:
+                print("well?")
+                body += "HTTP/1.1 200 OK\nContent-Type: text/css\n\n"
+                body += css.read()
+                css.close()
+        except:
+            print("yo that's a 404")
+            body += self.handle_404()
+        return body
+
+    # returns the body
+    def handle_get(self):
+
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        requested_path = current_path + "/www" + str(self.path, "utf-8")
+
+        # split by the /
+        split_path = requested_path.split("/")
+
+        # prevent root access
+        if "../" in requested_path:
+            return self.handle_404()
+
+        # 301 on *almost* correct path
+        if os.path.isdir(requested_path) and requested_path[-1] != "/":
+            return self.handle_301(split_path)
+
+        # time to check where we're going
+        print(split_path)
+        
+        body = ""
+
+        # this probably could be a switch statement
+        # but that is way above my paygrade
+        # requests to ""
+        if split_path[-1] == "":
+            print("main level!")
+            body += self.handle_html(requested_path + "/index.html")
+        # request to index.html
+        elif "html" in split_path[-1]:
+            print("index!")
+            body += self.handle_html(requested_path)
+
+        # time for css
+        elif "css" in requested_path:
+            body += self.handle_css(requested_path)
+
+        elif not os.path.isdir(requested_path):
+            return self.handle_404()
+
+        return body
+
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        self.data = self.data.split(b" ")
+        self.http_request = self.data[0]
+        self.path = self.data[1]
+
+        # GET received
+        if self.http_request == b"GET":
+            body = self.handle_get()
+            self.request.sendall(bytearray(body,'utf-8'))
+        # 405 when not GET
+        else:
+            body = self.handle_405()
+            self.request.sendall(bytearray(body,'utf-8'))
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -40,6 +155,8 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+
+    print("Now serving on {HOST}:{PORT}".format(HOST=HOST,PORT=PORT))
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
